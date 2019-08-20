62192f09132c8b7b693499d7a068657dbcd050e7
siaoynli <siaoynli@126.com>
2019-08-20 16:13:20 +0800

    ok

---
+++ src/LaravelAliCloudServerProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AliCloud\Core;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelAliCloudServerProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '../config/alicloud.php' => config_path('alicloud.php'),
+        ]);
+    }
+}
